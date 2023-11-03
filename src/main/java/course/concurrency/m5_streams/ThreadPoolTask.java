176dcbfd20f2097c0929041274aa33a3b2555514
Морозов Евгений Васильевич <Evgeny.Morozov@mvideo.ru>
2023-11-03 21:22:37 +0300

    Add Module 5 solution

--- src/main/java/course/concurrency/m5_streams/ThreadPoolTask.java
+++ src/main/java/course/concurrency/m5_streams/ThreadPoolTask.java
@@ -4,13 +4,33 @@ import java.util.concurrent.*;
 
 public class ThreadPoolTask {
 
+    private static class LifoBlockingQueue<E> extends LinkedBlockingDeque<E> {
+        @Override
+        public E take() throws InterruptedException {
+            return super.takeLast();
+        }
+    }
+    
     // Task #1
     public ThreadPoolExecutor getLifoExecutor() {
-        return null;
+        return new ThreadPoolExecutor(
+                1,
+                1,
+                0,
+                TimeUnit.MILLISECONDS,
+                new LifoBlockingQueue<>()
+        );
     }
 
     // Task #2
     public ThreadPoolExecutor getRejectExecutor() {
-        return null;
+        return new ThreadPoolExecutor(
+                1,
+                8,
+                60,
+                TimeUnit.SECONDS,
+                new SynchronousQueue<>(),
+                new ThreadPoolExecutor.DiscardPolicy()
+        );
     }
 }
